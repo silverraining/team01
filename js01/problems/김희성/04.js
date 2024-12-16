0cff5d55c6b7c59e4d34e11806452d4e8eef0e48
HEES56 <rlagmltjd125@gmail.com>
2024-12-16 18:27:55 +0900

    feat : 문제 구분화

---
+++ js01/problems/김희성/04.js
@@ -0,0 +1,17 @@
+// -영희의 건망증-
+// 영희는 잘 까먹어 상대방의 이름과 나이를 잘 기억하지 못해 호칭에 애를 썼다.
+// 그런 영희를 위해 아래 함수를 이용해 배열을 만들어 주자!
+
+function introduce (age, Yage, Oage) {
+}
+
+function Yage(age < 23) {
+  console.log("나보다 연하다!");
+}
+function Oage(age > 23) {
+  console.log("나보다 연상이다!");
+}
+
+const information1 = function introduce (20, Yage, Oage);
+const information2 = function introduce (26, Yage, Oage);
+
